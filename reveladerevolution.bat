aa44102154902c16506f673f26b0bc174943ec9c
wakadoodle <wakadoodle@77a44b20-d881-4ca4-83ca-358cc907d8a9>
2013-08-25 20:56:36 +0000

    Updated enet to 1.3.9 Added a "stable" preview to the game (binaries and startup scripts)

---
+++ reveladerevolution.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+set RR_BIN=bin
+IF /I "%PROCESSOR_ARCHITECTURE%" == "amd64" (
+set RR_BIN=bin64
+)
+IF /I "%PROCESSOR_ARCHITEW6432%" == "amd64" (
+set RR_BIN=bin64
+)
+start %RR_BIN%\reveladerevolution.exe "-q$HOME\My Games\ReveladeRevolution" -glog.txt %* 
