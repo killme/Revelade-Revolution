4444d40499f746176a68cff9277e5bdda9c2d844
theintercooler <bigfootzz@users.noreply.github.com>
2013-09-26 23:09:53 +0000

    Fixed point to of flags on blip from packages/hud to data/hud

--- src/fpsgame/client.cpp
+++ src/fpsgame/client.cpp
@@ -68,7 +68,7 @@ namespace game
             {
                 if(!alive++) 
                 {
-                    settexture(isteam(d->team, player1->team) ? "packages/hud/blip_blue_alive.png" : "packages/hud/blip_red_alive.png");
+                    settexture(isteam(d->team, player1->team) ? "data/hud/blip_blue_alive.png" : "data/hud/blip_red_alive.png");
                     glBegin(GL_QUADS);
                 }
                 drawteammate(d, x, y, s, o, scale);
@@ -82,7 +82,7 @@ namespace game
             {
                 if(!dead++) 
                 {
-                    settexture(isteam(d->team, player1->team) ? "packages/hud/blip_blue_dead.png" : "packages/hud/blip_red_dead.png");
+                    settexture(isteam(d->team, player1->team) ? "data/hud/blip_blue_dead.png" : "data/hud/blip_red_dead.png");
                     glBegin(GL_QUADS);
                 }
                 drawteammate(d, x, y, s, o, scale);
