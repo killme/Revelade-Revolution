1f308866ef163c0fc3ea537b67dc39ae21d08f0f
tempris <tempris@users.noreply.github.com>
2013-11-16 12:31:09 +0000

    No true mod here

--- RR_Game.bat
+++ RR_Game.bat
@@ -1,12 +1,31 @@
 @ECHO OFF
 
-set TESS_BIN=bin
+rem set SDL_VIDEO_WINDOW_POS=0,0
+set APP_DIR=.
+set APP_OPTIONS= "-q$HOME\My Games\ReveladeRevolution"
+set APP_BIN=bin
 
 IF /I "%PROCESSOR_ARCHITECTURE%" == "amd64" (
-    set TESS_BIN=bin
+    set APP_BIN=bin64
 )
 IF /I "%PROCESSOR_ARCHITEW6432%" == "amd64" (
-    set TESS_BIN=bin
+    set APP_BIN=bin64
 )
 
-start %TESS_BIN%\RR_GAME.exe
+:RETRY
+IF EXIST %APP_BIN%\reveladerevolution.exe (
+    start %APP_BIN%\reveladerevolution.exe %APP_OPTIONS% %*
+) ELSE (
+    IF EXIST %APP_DIR%\%APP_BIN%\reveladerevolution.exe (
+        pushd %APP_DIR%
+        start %APP_BIN%\reveladerevolution.exe %APP_OPTIONS% %*
+        popd
+    ) ELSE (
+        IF %APP_BIN% == bin64 (
+            set APP_BIN=bin
+            goto RETRY
+        )
+        echo Unable to find the Revelade Revolution client
+        pause
+    )
+)
