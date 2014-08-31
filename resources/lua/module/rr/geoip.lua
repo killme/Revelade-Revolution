a562323e9c42c35461981343b997e5b1bb65aa25
killme0 <killme@users.noreply.github.com>
2014-08-31 10:23:13 +0000

    Automatically disable geoip if the library cannot be loaded.

--- resources/lua/module/rr/geoip.lua
+++ resources/lua/module/rr/geoip.lua
@@ -1,27 +1,39 @@
 
-local Database = require "geoip".Database
+local geoip, Database
+
+local _, geoipError = pcall(function()
+    geoip = require "geoip"
+    Database = geoip.Database
+end)
+
 local BaseModule = require "server.module".BaseModule
 local Module = BaseModule:extend()
 
 function Module:load(config)
-    self.db = Database:new(config.path)
+    self.db = Database and Database:new(config.path)
 
-    self.handler = function(event)
-        local ip = self.serverHandle:getIp(event.cn)
-        local from = self.db:lookup(ip) or {}
-        self:sendMessage(event.cn, from)
+    if self.db then
+        self.serverHandle:print ("[geoip] Opened database: "..config.path)
+
+        self.handler = function(event)
+            local ip = self.serverHandle:getIp(event.cn)
+            local from = self.db:lookup(ip) or {}
+            self:sendMessage(event.cn, from)
+        end
+
+        self.serverHandle:on("client.connect", self.handler)
+
+        self:registerCommand("geoip.getIp", "PRIV_MODERATOR", function(cn)
+            cn = tonumber(cn)
+            if type(cn) ~= "number" then error("Invalid client number") end
+            p "exec getip"
+            return tostring(self.serverHandle:getIp(cn))
+        end)
+    else
+        self.serverHandle:printError("[geoip] Could not open geoip library: "..tostring(geoipError))
+        self.serverHandle:printError("[geoip] Geoip is disabled")
     end
 
-    self.serverHandle:print ("[geoip] Opened database: "..config.path)
-
-    self.serverHandle:on("client.connect", self.handler)
-
-    self:registerCommand("geoip.getIp", "PRIV_MODERATOR", function(cn)
-        cn = tonumber(cn)
-        if type(cn) ~= "number" then error("Invalid client number") end
-        p "exec getip"
-        return tostring(self.serverHandle:getIp(cn))
-    end)
 
     BaseModule.load(self, config)
 end
